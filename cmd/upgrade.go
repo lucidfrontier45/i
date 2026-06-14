f209c7f9fc62c1026a8514441f0d4a29a87a9a63
杜世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2026-06-15 00:54:38 +0900

    feat: add go driver using go install command (#4)
    Implements the types.Driver interface for Go toolchain packages managed via go install pkg@version. Supports install, upgrade, remove (binary deletion), installed version detection via go version -m, and auto-detection of Go toolchain.

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -115,5 +115,5 @@ var upgradeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(upgradeCmd)
-	upgradeCmd.Flags().String("manager", "", "Package manager (bun, uv, cargo, grd)")
+	upgradeCmd.Flags().String("manager", "", "Package manager (bun, uv, cargo, grd, go)")
 }
