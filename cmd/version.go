d4e1305a30e1ab3163ce79dc0addc6c8820f66a3
杜世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2026-06-15 11:37:33 +0900

    feat: add version subcommand to print version info (#12)

---
+++ cmd/version.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	Version = "dev"
+	Commit  = ""
+	Date    = ""
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Args:  cobra.NoArgs,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		fmt.Printf("i version %s\n", Version)
+		if Commit != "" {
+			fmt.Printf("commit %s\n", Commit)
+		}
+		if Date != "" {
+			fmt.Printf("built at %s\n", Date)
+		}
+		fmt.Printf("go version %s\n", runtime.Version())
+		fmt.Printf("os/arch %s/%s\n", runtime.GOOS, runtime.GOARCH)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
